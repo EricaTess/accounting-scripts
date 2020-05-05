400226960f2997d2d0b86675baf85f7160027a21
EricaTess <sanchez.ericat@gmail.com>
2020-05-05 17:13:34 +0000

    initial commit

---
+++ melon_info.py
@@ -0,0 +1,26 @@
+"""Print out all the melons in our inventory."""
+
+
+from melons import melons
+
+
+def print_melon(melons):
+    """Print each melon with corresponding attribute information."""
+
+    for melon_name, attributes in melons.items():
+        print(melon_name.upper())
+
+        for attribute, value in attributes.items():
+            print(f'{attribute}: {value}')
+
+print(print_melon(melons))
+
+#     # have_or_have_not = 'have'
+#     # if melons[seedless]:
+#     #     have_or_have_not = 'do not have'
+
+#     # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
---
+++ melons.py
@@ -0,0 +1,61 @@
+melon_names = {
+    1: 'Honeydew',
+    2: 'Crenshaw',
+    3: 'Crane',
+    4: 'Casaba',
+    5: 'Cantaloupe',
+}
+
+melon_prices = {
+    1: 0.99,
+    2: 2.00,
+    3: 2.50,
+    4: 2.50,
+    5: 0.99,
+}
+
+melon_seedlessness = {
+    1: True,
+    2: False,
+    3: False,
+    4: False,
+    5: False,
+}
+
+melons = {
+    'Honeydew': {
+    'seedless': True,
+    'prices': 0.99,
+    'flesh_color': None,
+    'weight': None,
+    'rind_color': None
+    },
+    'Crenshaw': {
+    'seedless': False,
+    'prices': 2.00,
+    'flesh_color': None,
+    'weight': None,
+    'rind_color': None
+    },
+    'Crane': {
+    'seedless': False,
+    'prices': 2.50,
+    'flesh_color': None,
+    'weight': None,
+    'rind_color': None
+    },
+    'Casaba': {
+    'seedless': False,
+    'prices': 2.50,
+    'flesh_color': None,
+    'weight': None,
+    'rind_color': None
+    },
+    'Cantaloupe': {
+    'seedless': False,
+    'prices': 0.99,
+    'flesh_color': None,
+    'weight': None,
+    'rind_color': None
+    }
+}
