6b35158c131658be7c408f2be99db72305158b19
EricaTess <sanchez.ericat@gmail.com>
2020-05-05 17:14:45 +0000

    removed commented out previous solution

--- melon_info.py
+++ melon_info.py
@@ -15,12 +15,4 @@ def print_melon(melons):
 
 print(print_melon(melons))
 
-#     # have_or_have_not = 'have'
-#     # if melons[seedless]:
-#     #     have_or_have_not = 'do not have'
 
-#     # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-
-
-# for i in melon_names:
-#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
